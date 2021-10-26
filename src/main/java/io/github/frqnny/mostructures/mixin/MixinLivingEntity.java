154a5107395a7da7696f95a4699119d4bc64a8bd
Jelle De Loecker <jelle@elevenways.be>
2021-10-26 10:28:00 +0200

    Disable rabbits dropping diamonds

--- src/main/java/io/github/frqnny/mostructures/mixin/MixinLivingEntity.java
+++ src/main/java/io/github/frqnny/mostructures/mixin/MixinLivingEntity.java
@@ -1,41 +1,42 @@
 package io.github.frqnny.mostructures.mixin;
 
 import io.github.frqnny.mostructures.util.ItemUtils;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.RabbitEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(LivingEntity.class)
-public class MixinLivingEntity {
+@Mixin(RabbitEntity.class)
+public abstract class MixinLivingEntity extends AnimalEntity {
 
 
-    @Inject(method = "onDeath", at = @At("TAIL"))
-    public void dropItems(DamageSource source, CallbackInfo info) {
+    @Shadow public abstract int getRabbitType();
 
-        if (((LivingEntity) (Object) this) instanceof RabbitEntity rabbit) {
-            if (rabbit.getRabbitType() == 99) {
-                ItemStack diamonds = new ItemStack(Items.DIAMOND);
-                diamonds.setCount(Math.max(8, rabbit.getRandom().nextInt(10)));
-                ItemUtils.spawnStack(rabbit.world, rabbit.getX(), rabbit.getY() + 0.5, rabbit.getZ(), diamonds);
+    protected MixinLivingEntity(EntityType<? extends AnimalEntity> entityType, World world) {
+        super(entityType, world);
+    }
 
-                ItemStack emeralds = new ItemStack(Items.EMERALD);
-                emeralds.setCount(Math.max(10, rabbit.getRandom().nextInt(12)));
-                ItemUtils.spawnStack(rabbit.world, rabbit.getX(), rabbit.getY() + 0.5, rabbit.getZ(), emeralds);
+    //@Inject(method = "onDeath", at = @At("TAIL"))
+    public void onDeath(DamageSource source) {
 
-                ItemStack ironIngots = new ItemStack(Items.IRON_INGOT);
-                ironIngots.setCount(Math.max(18, rabbit.getRandom().nextInt(20)));
-                ItemUtils.spawnStack(rabbit.world, rabbit.getX(), rabbit.getY() + 0.5, rabbit.getZ(), ironIngots);
+        if (this.getRabbitType() == 99) {
 
-                ItemStack goldIngots = new ItemStack(Items.IRON_INGOT);
-                goldIngots.setCount(Math.max(16, rabbit.getRandom().nextInt(18)));
-                ItemUtils.spawnStack(rabbit.world, rabbit.getX(), rabbit.getY() + 0.5, rabbit.getZ(), goldIngots);
-            }
+            ItemStack goldIngots = new ItemStack(Items.GOLDEN_CARROT);
+            goldIngots.setCount(Math.max(8, this.getRandom().nextInt(10)));
+            ItemUtils.spawnStack(this.world, this.getX(), this.getY() + 0.5, this.getZ(), goldIngots);
+
+            return;
         }
+
+        super.onDeath(source);
     }
 }
